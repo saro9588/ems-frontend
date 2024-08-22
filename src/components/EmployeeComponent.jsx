237c96c3c1f387b792f6e2ea4f381a6f877a974e
Saro <babikiansaro@gmail.com>
2024-08-22 12:43:58 -0700

    create-add-employee-form-

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EmployeeComponent = () => {
-  return <div>EmployeeComponent</div>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function saveEmployee(e) {
+    e.preventDefault();
+    const employee = { firstName, lastName, email };
+    console.log(employee);
+  }
+  return (
+    <div className="container">
+      <br />
+      <div className="row">
+        <div className="card col-md-6 offset-md-3 offset-md-3">
+          <h2 className="text-center">Add Emplyee</h2>
+          <div className="card-body">
+            <form>
+              <div className="form-group mb-2">
+                <label className="form-label">First Name</label>
+                <input
+                  type="text"
+                  placeholder="Enter employee first name"
+                  name="firstName"
+                  value={firstName}
+                  className="form-control"
+                  onChange={(e) => setFirstName(e.target.value)}
+                ></input>
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Last Name</label>
+                <input
+                  type="text"
+                  placeholder="Enter employee last name"
+                  name="lastName"
+                  value={lastName}
+                  className="form-control"
+                  onChange={(e) => setLastName(e.target.value)}
+                ></input>
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Email</label>
+                <input
+                  type="text"
+                  placeholder="Enter employee email"
+                  name="email"
+                  value={email}
+                  className="form-control"
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
+              </div>
+              <button className="btn btn-success" onClick={saveEmployee}>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeComponent;
