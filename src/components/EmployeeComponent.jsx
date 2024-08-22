ccc426d32dae524393327f0833775baa1b23b778
Saro <babikiansaro@gmail.com>
2024-08-22 14:28:38 -0700

    add-function-to-get-specific-employee-from-api-to-populate-updateEmplyee-page-fields

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { createEmployee } from "../services/EmployeeService";
+import React, { useEffect, useState } from "react";
+import { createEmployee, getEmployee } from "../services/EmployeeService";
 import { useNavigate, useParams } from "react-router-dom";
 
 const EmployeeComponent = () => {
@@ -13,6 +13,20 @@ const EmployeeComponent = () => {
   });
   const { id } = useParams();
   const navigator = useNavigate();
+
+  useEffect(() => {
+    if (id) {
+      getEmployee(id)
+        .then((response) => {
+          setFirstName(response.data.firstName);
+          setLastName(response.data.lastName);
+          setEmail(response.data.email);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [id]);
 
   function saveEmployee(e) {
     e.preventDefault();
